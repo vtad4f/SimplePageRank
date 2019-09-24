d84ba808902b667d047a3d163fff06569c9ff0b0
Vincent Troy Allen Jr <vtad4f@mst.edu>
2019-09-23 19:09:18 -0500

    Needed more backslashes

--- src/PageRank.java
+++ src/PageRank.java
@@ -14,58 +14,58 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class PageRank {
 
-	public static class MyMapper extends Mapper<Object, Text, Text, DoubleWritable> {
+   public static class MyMapper extends Mapper<Object, Text, Text, DoubleWritable> {
 
-		@Override
-		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+      @Override
+      public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 
-			String[] pages = value.toString().split("[\\s\"\[\]',]");
+         String[] pages = value.toString().split("[\\s\"\\[\\]',]");
          
          double d = .85;
-			double initRank = 1.0;
-			double ratio = d * initRank / (pages.length - 1);	
+         double initRank = 1.0;
+         double ratio = d * initRank / (pages.length - 1);   
          
-			context.write(new Text(pages[0]), new DoubleWritable(initRank - d));
-			
-			for (int i = 1; i < pages.length; i++) {
-				context.write(new Text(pages[i]), new DoubleWritable(ratio));
-			}
-		}
-	}
+         context.write(new Text(pages[0]), new DoubleWritable(initRank - d));
+         
+         for (int i = 1; i < pages.length; i++) {
+            context.write(new Text(pages[i]), new DoubleWritable(ratio));
+         }
+      }
+   }
 
-	public static class MyReducer extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
+   public static class MyReducer extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
 
-		@Override
-		public void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
-			
-			double pagerank = 0.0;
-			for (DoubleWritable value : values) {
-				pagerank += Double.parseDouble(value.toString());
-			}
-			context.write(key, new DoubleWritable(pagerank));
-		}
-	}
+      @Override
+      public void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
+         
+         double pagerank = 0.0;
+         for (DoubleWritable value : values) {
+            pagerank += Double.parseDouble(value.toString());
+         }
+         context.write(key, new DoubleWritable(pagerank));
+      }
+   }
 
-	public static void main(String[] args) throws Exception {
+   public static void main(String[] args) throws Exception {
 
-		Job job = Job.getInstance(new Configuration());
-		job.setJarByClass(PageRank.class);
-		job.setJobName("Page Rank");
-		
-		job.setMapperClass(MyMapper.class);
-		job.setReducerClass(MyReducer.class);
-		
-		job.setInputFormatClass(TextInputFormat.class);
-		job.setOutputFormatClass(TextOutputFormat.class);
-		
-		FileInputFormat.setInputPaths(job, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-		
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(DoubleWritable.class);
-		
-		job.setNumReduceTasks(1);
+      Job job = Job.getInstance(new Configuration());
+      job.setJarByClass(PageRank.class);
+      job.setJobName("Page Rank");
+      
+      job.setMapperClass(MyMapper.class);
+      job.setReducerClass(MyReducer.class);
+      
+      job.setInputFormatClass(TextInputFormat.class);
+      job.setOutputFormatClass(TextOutputFormat.class);
+      
+      FileInputFormat.setInputPaths(job, new Path(args[0]));
+      FileOutputFormat.setOutputPath(job, new Path(args[1]));
+      
+      job.setOutputKeyClass(Text.class);
+      job.setOutputValueClass(DoubleWritable.class);
+      
+      job.setNumReduceTasks(1);
 
-		System.exit(job.waitForCompletion(true) ? 0 : 1);
-	}
+      System.exit(job.waitForCompletion(true) ? 0 : 1);
+   }
 }
