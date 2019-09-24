232ddab15aeb77c54b493ebfdbbe6b4f77744bdb
Vincent Allen <vtadf@mst.edu>
2019-09-24 02:31:10 +0100

    Compiles but doesn't run

--- src/PageRank.java
+++ src/PageRank.java
@@ -14,58 +14,76 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class PageRank {
 
-   public static class MyMapper extends Mapper<Object, Text, Text, DoubleWritable> {
+	public static class MyMapper extends Mapper<Object, Text, Text, DoubleWritable[]> {
 
-      @Override
-      public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+		@Override
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 
-         String[] pages = value.toString().split("[\\s\"\\[\\]',]");
-         
-         double d = .85;
-         double initRank = 1.0;
-         double ratio = d * initRank / (pages.length - 1);   
-         
-         context.write(new Text(pages[0]), new DoubleWritable(initRank - d));
-         
-         for (int i = 1; i < pages.length; i++) {
-            context.write(new Text(pages[i]), new DoubleWritable(ratio));
-         }
-      }
-   }
+			String[] pages = value.toString().split("[\\s\"\\[\\]',]");
+			
+			double d = .85;
+			double initRank = 1.0;
+			double ratio = d * initRank / (pages.length - 1);	
+			
+			DoubleWritable[] p0 = {
+				new DoubleWritable(initRank - d),
+				new DoubleWritable(pages.length - 1),
+				new DoubleWritable(0)
+			};
+			DoubleWritable[] pn = {
+				new DoubleWritable(ratio),
+				new DoubleWritable(0),
+				new DoubleWritable(1)
+			};
+			
+			context.write(new Text(pages[0]), p0);
 
-   public static class MyReducer extends Reducer<Text, DoubleWritable, Text, DoubleWritable> {
+			for (int i = 1; i < pages.length; i++) {
+				context.write(new Text(pages[i]), pn);
+			}
+		}
+	}
 
-      @Override
-      public void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
-         
-         double pagerank = 0.0;
-         for (DoubleWritable value : values) {
-            pagerank += Double.parseDouble(value.toString());
-         }
-         context.write(key, new DoubleWritable(pagerank));
-      }
-   }
+	public static class MyReducer extends Reducer<Text, DoubleWritable[], Text, DoubleWritable[]> {
 
-   public static void main(String[] args) throws Exception {
+		@Override
+		public void reduce(Text key, Iterable<DoubleWritable[]> values, Context context) throws IOException, InterruptedException {
+			
+			double[] total = {0.0, 0.0, 0.0};
+			for (DoubleWritable[] value : values) {
+				total[0] += Double.parseDouble(value[0].toString());
+				total[1] += Double.parseDouble(value[1].toString());
+				total[2] += Double.parseDouble(value[2].toString());
+			}
+			DoubleWritable[] output = {
+				new DoubleWritable(total[0]),
+				new DoubleWritable(total[1]),
+				new DoubleWritable(total[2])
+			};
+			context.write(key, output);
+		}
+	}
 
-      Job job = Job.getInstance(new Configuration());
-      job.setJarByClass(PageRank.class);
-      job.setJobName("Page Rank");
-      
-      job.setMapperClass(MyMapper.class);
-      job.setReducerClass(MyReducer.class);
-      
-      job.setInputFormatClass(TextInputFormat.class);
-      job.setOutputFormatClass(TextOutputFormat.class);
-      
-      FileInputFormat.setInputPaths(job, new Path(args[0]));
-      FileOutputFormat.setOutputPath(job, new Path(args[1]));
-      
-      job.setOutputKeyClass(Text.class);
-      job.setOutputValueClass(DoubleWritable.class);
-      
-      job.setNumReduceTasks(1);
+	public static void main(String[] args) throws Exception {
 
-      System.exit(job.waitForCompletion(true) ? 0 : 1);
-   }
+		Job job = Job.getInstance(new Configuration());
+		job.setJarByClass(PageRank.class);
+		job.setJobName("Page Rank");
+		
+		job.setMapperClass(MyMapper.class);
+		job.setReducerClass(MyReducer.class);
+		
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		
+		FileInputFormat.setInputPaths(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(DoubleWritable[].class);
+		
+		job.setNumReduceTasks(1);
+
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
 }
